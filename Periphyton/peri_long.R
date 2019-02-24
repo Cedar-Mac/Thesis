134f4ef98ecd2f8521ba17b895044a3437d0610d
Cedar Mackaness <cedarmks@gmail.com>
2019-02-24 15:34:08 -0800

    Adjusting figs for poster

--- Periphyton/peri_long.R
+++ Periphyton/peri_long.R
@@ -1,42 +1,73 @@
 # Prep Periphyton data for PC ORD
-x <- c("tidyverse", "vegan", "lubridate", "readxl", "ggthemes", "vegan", "viridis", "DT")
+x <- c("tidyverse", "vegan", "lubridate", "readxl", "ggthemes", "viridis")
 lapply(x, library, character.only = TRUE)
 
 reach.peri <- read_csv("../Periphyton/Data/Reach_Algae_DiatomsSpecified_pca_data.csv")
 meter.peri <- read_csv("../Periphyton/Data/Meter_Algae_DiatomsSpecified_pca_data.csv")
  
+
+
 reach.cell <- reach.peri %>% 
   select(-c("Sum_Count", "Sum_Het")) %>% 
   spread(key = Portion.Taxon, value = Sum_CellCount) %>%
-  mutate_if(is.numeric , replace_na, replace = 0) %>% as.data.frame
+  mutate_if(is.numeric , replace_na, replace = 0) %>% 
+  group_by(Time.Reach) %>%
+  summarise_if(is.numeric, funs(sum)) %>%
+  select(-c("X1", "Time.Reach")) 
 
-reach.het <-  reach.peri %>% 
+
+reach.het <- reach.peri %>% 
   select(-c("Sum_Count", "Sum_CellCount")) %>% 
   spread(key = Portion.Taxon, value = Sum_Het) %>% 
-  mutate_if(is.numeric , replace_na, replace = 0) %>% as.data.frame                         
+  mutate_if(is.numeric , replace_na, replace = 0) %>% 
+  group_by(Time.Reach) %>%
+  summarise_if(is.numeric, funs(sum)) %>%
+  select(-c("X1", "Time.Reach"))
+                           
            
 reach.count <-  reach.peri %>% 
   select(-c("Sum_Het", "Sum_CellCount")) %>% 
   spread(key = Portion.Taxon, value = Sum_Count) %>% 
-  mutate_if(is.numeric , replace_na, replace = 0) %>% as.data.frame    
+  mutate_if(is.numeric , replace_na, replace = 0) %>% 
+  group_by(Time.Reach) %>%
+  summarise_if(is.numeric, funs(sum)) %>%
+  select(-c("X1", "Time.Reach"))
+
 
 meter.count <-  meter.peri %>% 
   select(-c("Sum_Het", "Sum_CellCount")) %>% 
   spread(key = Portion.Taxon, value = Sum_Count) %>% 
-  mutate_if(is.numeric , replace_na, replace = 0) %>% as.data.frame 
+  mutate_if(is.numeric , replace_na, replace = 0) %>% 
+  group_by(Time.Reach.Meter) %>%
+  summarise_if(is.numeric, funs(sum)) %>%
+  select(-c("X1", "Time.Reach.Meter"))
+  
 
 meter.het <-  meter.peri %>% 
   select(-c("Sum_Count", "Sum_CellCount")) %>% 
   spread(key = Portion.Taxon, value = Sum_Het) %>% 
-  mutate_if(is.numeric , replace_na, replace = 0) %>% as.data.frame
+  mutate_if(is.numeric , replace_na, replace = 0) %>% 
+  group_by(Time.Reach.Meter) %>%
+  summarise_if(is.numeric, funs(sum)) %>%
+  select(-c("X1", "Time.Reach.Meter"))
 
 meter.cell <-  meter.peri %>% 
   select(-c("Sum_Het", "Sum_Count")) %>% 
   spread(key = Portion.Taxon, value = Sum_CellCount) %>% 
-  mutate_if(is.numeric , replace_na, replace = 0) %>% as.data.frame
+  mutate_if(is.numeric , replace_na, replace = 0) %>% 
+  group_by(Time.Reach.Meter) %>%
+  summarise_if(is.numeric, funs(sum)) %>%
+  select(-c("X1", "Time.Reach.Meter"))
 
-meter.env <- select(meter.cell, c("X1", "Time.Reach.Meter"))
-reach.env <- select(reach.cell, c("X1", "Time.Reach"))
+reach.env <- reach.peri %>% 
+  select(-c("Sum_Count", "Sum_Het")) %>% 
+  spread(key = Portion.Taxon, value = Sum_CellCount) %>% 
+  select(Time.Reach) %>% unique()
+
+meter.env <- meter.peri %>% 
+  select(-c("Sum_Het", "Sum_Count")) %>% 
+  spread(key = Portion.Taxon, value = Sum_CellCount) %>% 
+  select(Time.Reach.Meter) %>% unique()
 
 
 
@@ -45,3 +76,59 @@ peri.list <- list(meter.cell = meter.cell, meter.het = meter.het, meter.count = 
                   meter.env = meter.env, reach.env = reach.env)
 
 mapply(write.csv, peri.list, file = paste0(names(peri.list), '.csv'))
+
+
+#### By METER
+
+Density <- beals(meter.count)
+Enviro <- meter.env
+sol <- metaMDS(Density, distance = "bray", k = 2, trymax = 100)
+
+NMDS <- data.frame(x = sol$points[, 1], y = sol$points[ ,2], 
+                   Time.Reach.Meter = select(Enviro, Time.Reach.Meter))
+
+plot.new()
+ord <- ordiellipse(sol, NMDS$Time.Reach.Meter, display = "sites", kind = "sd", conf = 0.95, label = TRUE)
+dev.off()
+
+df_ell <- data.frame()
+for(g in NMDS$Time.Reach.Meter){
+  df_ell <- rbind(df_ell, cbind(as.data.frame(with(NMDS[NMDS$Time.Reach.Meter == g,],
+                                                   vegan:::veganCovEllipse(ord[[g]]$cov, ord[[g]]$center, ord[[g]]$scale)))
+                                ,Time.Reach.Meter = g))
+}
+
+ggplot(data = NMDS, aes(x, y, color = Time.Reach.Meter)) +
+  geom_path(data = df_ell, aes(x = NMDS1, y = NMDS2)) +
+  annotate("text", x = (NMDS$x), y = (NMDS$y), label = NMDS$Time.Reach.Meter, size = 2) +
+  geom_point() +
+  ggtitle("NMDS of Periphyton Community By Meter") +
+  scale_color_viridis_d()
+
+#### By REACH
+
+Density <- reach.count
+Enviro <- reach.env
+sol <- metaMDS(Density, distance = "bray", k = 2, trymax = 100)
+
+NMDS <- data.frame(x = sol$points[, 1], y = sol$points[ ,2], 
+                   Time.Reach = select(Enviro, Time.Reach))
+
+plot.new()
+ord <- ordiellipse(sol, NMDS$Time.Reach, display = "sites", kind = "sd", conf = 0.95, label = TRUE)
+dev.off()
+
+df_ell <- data.frame()
+for(g in NMDS$Time.Reach){
+  df_ell <- rbind(df_ell, cbind(as.data.frame(with(NMDS[NMDS$Time.Reach == g,],
+                                                   vegan:::veganCovEllipse(ord[[g]]$cov, ord[[g]]$center, ord[[g]]$scale)))
+                                ,Time.Reach = g))
+}
+
+ggplot(data = NMDS, aes(x, y, color = Time.Reach)) +
+  geom_path(data = df_ell, aes(x = NMDS1, y = NMDS2)) +
+  annotate("text", x = (NMDS$x), y = (NMDS$y) + .04, label = NMDS$Time.Reach, size = 2) +
+  geom_point() +
+  ggtitle("NMDS of Periphyton Community By Reach") +
+  scale_color_viridis_d()
+
